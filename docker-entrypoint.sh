08637d0e023c75991da436d5d396b0af98ec3eb6
Dan Skadra <dskadra@gmail.com>
2018-09-10 16:06:20 -0400

    add support to configure environment timeout

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -55,14 +55,14 @@ if [[ "$1" = "puppetserver" ]]; then
   # Generate SSH key pair for R10k if it doesn't exist
   if [[ ! -f  /etc/puppetlabs/ssh/id_rsa ]]; then
     gen-ssh-keys -n -c "r10k-$(facter fqdn)"
-    if [[ $SHOW_SSH_KEY = "true" ]]; then
+    if [[ ${SHOW_SSH_KEY} = "true" ]]; then
       echo "SSH public key:"
       gen-ssh-keys -p
     fi
   fi
 
   # Configure for puppetdb if PUPPETDB_SERVER_URLS is set
-  if [[ -n "$PUPPETDB_SERVER_URLS" ]]; then
+  if [[ -n "${PUPPETDB_SERVER_URLS}" ]]; then
     echo "[main]" > /etc/puppetlabs/puppet/puppetdb.conf
     echo "server_urls = ${PUPPETDB_SERVER_URLS}" >> /etc/puppetlabs/puppet/puppetdb.conf
 
@@ -79,7 +79,7 @@ if [[ "$1" = "puppetserver" ]]; then
   fi
 
   # Disable strict host checking in SSH if SSH_HOST_KEY_CHECK is false
-  if [[ "$SSH_HOST_KEY_CHECK" = "false" ]]; then
+  if [[ "${SSH_HOST_KEY_CHECK}" = "false" ]]; then
     echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
   fi
 
@@ -90,7 +90,7 @@ if [[ "$1" = "puppetserver" ]]; then
     # If R10k sources are supplied via R10K_SOURCE* environment variables, add them to the r10k config file
     env -0 | while IFS='=' read -r -d '' NAME VALUE; do
       # looping through each R10K_SOURCE variables (R10K_SOURCE1, R10K_SOURCE2, etc)
-      if [[ $NAME =~ R10K_SOURCE\n* && -n "${VALUE}" ]]; then
+      if [[ ${NAME} =~ R10K_SOURCE\n* && -n "${VALUE}" ]]; then
         IFS=',' read -ra SOURCE <<< "$VALUE"
 
         echo -e "  ${SOURCE[0]}:\n    remote: ${SOURCE[1]}" >>/etc/puppetlabs/r10k/r10k.yaml
@@ -102,11 +102,11 @@ if [[ "$1" = "puppetserver" ]]; then
         fi
 
         # If SSH host key checking and auto trust is turned on, connect to source and add host key
-        if [[ "$SSH_HOST_KEY_CHECK" = "true" && "$TRUST_SSH_FIRST_CONNECT" = "true" ]]; then
+        if [[ ! "${SSH_HOST_KEY_CHECK}" = "false" && "${TRUST_SSH_FIRST_CONNECT}" = "true" ]]; then
           # Parse the R10K_SOURCE# url for the remote
           shopt -s nocasematch
           pattern='^(([[:alnum:]]+)://)?((([[:alnum:]]+)(:?([[:alnum:]]+)?))@)?([^:^@^/]+)(:([[:digit:]]+))?(/.*)'
-          if [[ ${SOURCE[1]} =~ $pattern ]]; then
+          if [[ ${SOURCE[1]} =~ ${pattern} ]]; then
             protocol=${BASH_REMATCH[2]}
             user=${BASH_REMATCH[5]}
             password=${BASH_REMATCH[7]}
@@ -116,16 +116,16 @@ if [[ "$1" = "puppetserver" ]]; then
           fi
 
           # Verify that the source URL is SSH
-          if [[ "$protocol" = 'ssh' ]]; then
+          if [[ "${protocol}" = 'ssh' ]]; then
             # set default ssh port of 22 if not indicated in the url
             port=${port:-22}
 
             # Check to see if the host already exists in known_hosts
             #   * known_hosts formats differently if alternate port specified
-            if [[ "$port" = "22" && ! "$(ssh-keygen -F $host -f /etc/puppetlabs/ssh/known_hosts -t rsa > /dev/null 2>&1)" ]]; then
-              ssh-keyscan -p $port $host >> /etc/puppetlabs/ssh/known_hosts
-            elif [[ ! "$(ssh-keygen -F [$host]:$port -f /etc/puppetlabs/ssh/known_hosts -t rsa > /dev/null 2>&1)" ]]; then
-              ssh-keyscan -p $port $host >> /etc/puppetlabs/ssh/known_hosts
+            if [[ "${port}" = "22" && ! "$(ssh-keygen -F ${host} -f /etc/puppetlabs/ssh/known_hosts -t rsa > /dev/null 2>&1)" ]]; then
+              ssh-keyscan -p ${port} ${host} >> /etc/puppetlabs/ssh/known_hosts
+            elif [[ ! "$(ssh-keygen -F [$host]:${port} -f /etc/puppetlabs/ssh/known_hosts -t rsa > /dev/null 2>&1)" ]]; then
+              ssh-keyscan -p ${port} $host >> /etc/puppetlabs/ssh/known_hosts
             fi
           fi
 
@@ -135,9 +135,14 @@ if [[ "$1" = "puppetserver" ]]; then
     done
   fi
 
+  # Should  be either 0 or unlimited. 0 is default, unlimited requires calling environment refresh API
+  if [[ -n "${ENVIRONMENT_TIMEOUT}" ]]; then
+    puppet config set environment_timeout ${ENVIRONMENT_TIMEOUT}
+  fi
+
   # Run R10k to update local environments if environment var set
   # r10k deploy environment -p -v
-  if [[ "$R10K_ON_STARTUP" = "true" ]]; then
+  if [[ "${R10K_ON_STARTUP}" = "true" ]]; then
     r10k deploy environment -p -v
   fi
 
