428f2e164ed9f20d9256af625f801caed3c94e49
Dan Skadra <dskadra@gmail.com>
2018-10-25 19:29:52 -0400

    cleanup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -eo pipefail
-if [[ -v DEBUG ]]; then set -x; fi
+if [ -v DEBUG ]; then set -x; fi
 
-if [[ "$2" = "foreground" ]]; then
+if [ "$2" = "foreground" ]; then
   # Update puppetserver configs to use JAVA_ARGS variable to configure java runtime
   [ -n "${JAVA_ARGS}" ] && sed -i "s/JAVA_ARGS=.*$/JAVA_ARGS=\"\$JAVA_ARGS\"/" /etc/sysconfig/puppetserver
 
   # Point the server's puppet agent to SERVER or this host's hostname
-  if [[ -n "${SERVER}" ]]; then
+  if [ -n "${SERVER}" ]; then
     puppet config set --section agent server ${SERVER}
   else
     puppet config set --section agent server $(facter hostname)
@@ -23,7 +23,7 @@ if [[ "$2" = "foreground" ]]; then
   puppet config set --section agent environment ${AGENT_ENVIRONMENT}
 
   # Enable basic autosigning
-  if [[ -n "${AUTOSIGN}" ]] ; then
+  if [ -n "${AUTOSIGN}" ] ; then
     echo "*" > /etc/puppetlabs/puppet/autosign.conf
     chown puppet.puppet /etc/puppetlabs/puppet/autosign.conf
   fi
@@ -31,11 +31,11 @@ if [[ "$2" = "foreground" ]]; then
   puppet config set --section main dns_alt_names $(facter fqdn),$(facter hostname),$DNS_ALT_NAMES
 
   # If the local CA server is disabled, configure the CA server host and port (optionally)
-  if [[ "${DISABLE_CA_SERVER}" = "true" && -n "${CA_SERVER}" ]]; then
+  if [ "${DISABLE_CA_SERVER}" = "true" && -n "${CA_SERVER}" ]; then
     sed -i "s/^\([^#].*certificate-authority-service\)/#\1/" /etc/puppetlabs/puppetserver/services.d/ca.cfg
     sed -i "s/^#\(.*certificate-authority-disabled-service\)/\1/" /etc/puppetlabs/puppetserver/services.d/ca.cfg
     puppet config set --section main ca_server "${CA_SERVER}"
-    if [[ -n "${CA_PORT}" ]]; then
+    if [ -n "${CA_PORT}" ]; then
       puppet config set --section main ca_port "${CA_PORT}"
     fi
 
@@ -52,16 +52,16 @@ if [[ "$2" = "foreground" ]]; then
   fi
 
   # Generate SSH key pair for R10k if it doesn't exist
-  if [[ ! -f  /etc/puppetlabs/ssh/id_rsa ]]; then
+  if [ ! -f  /etc/puppetlabs/ssh/id_rsa ]; then
     gen-ssh-keys -n -c "r10k-$(facter fqdn)"
-    if [[ ${SHOW_SSH_KEY} = "true" ]]; then
+    if [ ${SHOW_SSH_KEY} = "true" ]; then
       echo "SSH public key:"
       gen-ssh-keys -p
     fi
   fi
 
   # Configure for puppetdb if PUPPETDB_SERVER_URLS is set
-  if [[ -n "${PUPPETDB_SERVER_URLS}" ]]; then
+  if [ -n "${PUPPETDB_SERVER_URLS}" ]; then
     echo "[main]" > /etc/puppetlabs/puppet/puppetdb.conf
     echo "server_urls = ${PUPPETDB_SERVER_URLS}" >> /etc/puppetlabs/puppet/puppetdb.conf
     echo "soft_write_failure = ${SOFT_WRITE_FAILURES}" >> /etc/puppetlabs/puppet/puppetdb.conf
@@ -78,34 +78,34 @@ if [[ "$2" = "foreground" ]]; then
   fi
 
   # Disable strict host checking in SSH if SSH_HOST_KEY_CHECK is false
-  if [[ "${SSH_HOST_KEY_CHECK}" = "false" ]]; then
+  if [ "${SSH_HOST_KEY_CHECK}" = "false" ]; then
     echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
   fi
 
   # If r10k.yaml doesn't exist, build the basic r10k config file
-  if [[ ! -f /etc/puppetlabs/r10k/r10k.yaml ]]; then
+  if [ ! -f /etc/puppetlabs/r10k/r10k.yaml ]; then
     echo -e "---\n:cachedir: /opt/puppetlabs/server/data/puppetserver/r10k\n\n:sources:" > /etc/puppetlabs/r10k/r10k.yaml
 
     # If R10k sources are supplied via R10K_SOURCE* environment variables, add them to the r10k config file
     env -0 | while IFS='=' read -r -d '' NAME VALUE; do
       # looping through each R10K_SOURCE variables (R10K_SOURCE1, R10K_SOURCE2, etc)
-      if [[ ${NAME} =~ R10K_SOURCE\n* && -n "${VALUE}" ]]; then
+      if [ ${NAME} =~ R10K_SOURCE\n* && -n "${VALUE}" ]; then
         IFS=',' read -ra SOURCE <<< "$VALUE"
 
         echo -e "  ${SOURCE[0]}:\n    remote: ${SOURCE[1]}" >>/etc/puppetlabs/r10k/r10k.yaml
         echo -e "    basedir: /etc/puppetlabs/code/environments" >>/etc/puppetlabs/r10k/r10k.yaml
-        if [[ ${#SOURCE[@]} > 2 ]]; then
+        if [ ${#SOURCE[@]} > 2 ]; then
           echo -e "    prefix: ${SOURCE[2]}\n" >>/etc/puppetlabs/r10k/r10k.yaml
         else
           echo -e "    prefix: false\n" >>/etc/puppetlabs/r10k/r10k.yaml
         fi
 
         # If SSH host key checking and auto trust is turned on, connect to source and add host key
-        if [[ ! "${SSH_HOST_KEY_CHECK}" = "false" && "${TRUST_SSH_FIRST_CONNECT}" = "true" ]]; then
+        if [ ! "${SSH_HOST_KEY_CHECK}" = "false" && "${TRUST_SSH_FIRST_CONNECT}" = "true" ]; then
           # Parse the R10K_SOURCE# url for the remote
           shopt -s nocasematch
           pattern='^(([[:alnum:]]+)://)?((([[:alnum:]]+)(:?([[:alnum:]]+)?))@)?([^:^@^/]+)(:([[:digit:]]+))?(/.*)'
-          if [[ ${SOURCE[1]} =~ ${pattern} ]]; then
+          if [ ${SOURCE[1]} =~ ${pattern} ]; then
             protocol=${BASH_REMATCH[2]}
             user=${BASH_REMATCH[5]}
             password=${BASH_REMATCH[7]}
@@ -115,15 +115,15 @@ if [[ "$2" = "foreground" ]]; then
           fi
 
           # Verify that the source URL is SSH
-          if [[ "${protocol}" = 'ssh' ]]; then
+          if [ "${protocol}" = 'ssh' ]; then
             # set default ssh port of 22 if not indicated in the url
             port=${port:-22}
 
             # Check to see if the host already exists in known_hosts
             #   * known_hosts formats differently if alternate port specified
-            if [[ "${port}" = "22" && ! "$(ssh-keygen -F ${host} -f /etc/puppetlabs/ssh/known_hosts -t rsa > /dev/null 2>&1)" ]]; then
+            if [ "${port}" = "22" && ! "$(ssh-keygen -F ${host} -f /etc/puppetlabs/ssh/known_hosts -t rsa > /dev/null 2>&1)" ]; then
               ssh-keyscan -p ${port} ${host} >> /etc/puppetlabs/ssh/known_hosts
-            elif [[ ! "$(ssh-keygen -F [$host]:${port} -f /etc/puppetlabs/ssh/known_hosts -t rsa > /dev/null 2>&1)" ]]; then
+            elif [ ! "$(ssh-keygen -F [$host]:${port} -f /etc/puppetlabs/ssh/known_hosts -t rsa > /dev/null 2>&1)" ]; then
               ssh-keyscan -p ${port} $host >> /etc/puppetlabs/ssh/known_hosts
             fi
           fi
@@ -135,18 +135,18 @@ if [[ "$2" = "foreground" ]]; then
   fi
 
   # Should  be either 0 or unlimited. 0 is default, unlimited requires calling environment refresh API
-  if [[ -n "${ENVIRONMENT_TIMEOUT}" ]]; then
+  if [ -n "${ENVIRONMENT_TIMEOUT}" ]; then
     puppet config set environment_timeout ${ENVIRONMENT_TIMEOUT}
   fi
 
   # Run R10k to update local environments if environment var set
   # r10k deploy environment -p -v
-  if [[ "${R10K_ON_STARTUP}" = "true" ]]; then
+  if [ "${R10K_ON_STARTUP}" = "true" ]; then
     r10k deploy environment -p -v
   fi
 
   # Apply current config for this instance. Volumes retain config across container restarts
-  if [[ "${RUN_PUPPET_AGENT_ON_START}" = "true" ]]; then
+  if [ "${RUN_PUPPET_AGENT_ON_START}" = "true" ]; then
     puppet apply /etc/puppetlabs/code/environments/${AGENT_ENVIRONMENT}/manifests/site.pp -v
   fi
 fi
