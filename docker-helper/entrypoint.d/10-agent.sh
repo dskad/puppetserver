fbfe94c74f2bd1441040ae71b81dee0b3243a5e6
Unknown <dskadra@gmail.com>
2019-05-16 12:16:12 -0400

    typos and wording changes

--- docker-helper/entrypoint.d/10-agent.sh
+++ docker-helper/entrypoint.d/10-agent.sh
@@ -2,22 +2,23 @@
 
 # *** Puppet Agent Config ***
 #*********************
-# Point the server's puppet agent to SERVER or this host's hostname
+# Point the server's puppet agent to $SERVER or this host's hostname
 if [[ -n "${SERVER}" ]]; then
   puppet config set --section agent server ${SERVER}
 else
   puppet config set --section agent server $(facter hostname)
 fi
 
-# Set this for the agent to talk to a puppet master on a different port
+# Optionally set puppet server port for agent
 [[ -n "${MASTERPORT}" ]] && puppet config set --section agent masterport ${MASTERPORT}
 
-# Manually set a cert name, default is the container's fqdn/hash. (it's different every run!)
+# Optionally set a cert name, default is the container's fqdn/hash. (it's usually different every run!)
 [[ -n "${CERTNAME}" ]] && puppet config set --section main certname ${CERTNAME}
 
-# environment to configure this container
+# Set the puppet environment for the agent in this container. (default is production)
 puppet config set --section agent environment ${AGENT_ENVIRONMENT}
 
+# Optionally configure an external CA server
 # Have to be careful not to change the server's cert when acting as a CA server
 if [[ ! -n "${CA_SERVER}" ]]; then
   currentCertName=$(puppet config print certname)
@@ -30,6 +31,6 @@ if [[ ! -n "${CA_SERVER}" ]]; then
   fi
 else
   # We're not the CA server, we call ourselves anything we want!
-  puppet config set --section main dns_alt_names $(facter fqdn),$(facter hostname)${$DNS_ALT_NAMES:+,}$DNS_ALT_NAMES
+  puppet config set --section main dns_alt_names $(facter fqdn),$(facter hostname)${DNS_ALT_NAMES:+,}$DNS_ALT_NAMES
 fi
 
