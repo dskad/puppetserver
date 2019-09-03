0214017450520a10b05f24808eb14a09fe69bb15
Dan Skadra <dskadra@gmail.com>
2019-09-02 21:41:42 -0400

    add double quotes  arround variable substitutions

--- docker-helper/healthcheck.sh
+++ docker-helper/healthcheck.sh
@@ -4,9 +4,9 @@ set -e
 certname=$(puppet config print certname) && \
 curl -sS --fail -H 'Accept: pson' \
   --resolve "${certname}:8140:127.0.0.1" \
-  --cert   /etc/puppetlabs/puppet/ssl/certs/${certname}.pem \
-  --key    /etc/puppetlabs/puppet/ssl/private_keys/${certname}.pem \
+  --cert   "/etc/puppetlabs/puppet/ssl/certs/${certname}.pem" \
+  --key    "/etc/puppetlabs/puppet/ssl/private_keys/${certname}.pem" \
   --cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem \
-  https://${certname}:8140/${HEALTHCHECK_ENVIRONMENT}/status/test | \
+  "https://${certname}:8140/${HEALTHCHECK_ENVIRONMENT}/status/test" | \
   grep -q '"is_alive":true' || \
   exit 1
